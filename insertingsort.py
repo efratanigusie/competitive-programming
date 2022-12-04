7103aca98ec594a9a619036e3be165eaf228887d
efratanigusie <119038187+efratanigusie@users.noreply.github.com>
2022-12-04 17:11:52 +0300

    Create insertingsort.py

---
+++ insertingsort.py
@@ -0,0 +1,18 @@
+def insertionSort1(n, arr):
+    key=arr[-1]
+    i= n-1
+    
+    while i > 0 and arr[i-1] > key:
+        arr [i] = arr [i-1]
+        print(*arr)
+        i-=1
+    arr[i] = key
+    print(*arr)
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
