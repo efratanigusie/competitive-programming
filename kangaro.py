93f785c4f3dc7f6d452173a999f63a31c0ff8775
efratanigusie <119038187+efratanigusie@users.noreply.github.com>
2022-12-05 20:21:43 +0300

    Create kangaro.py

---
+++ kangaro.py
@@ -0,0 +1,29 @@
+ if (v1>v2):
+ def kangaroo(x1, v1, x2, v2):
+         r =(x1-x2) % (v2-v1)
+        if r==0:
+            print('no')
+        else:
+            print('no')
+    else:
+        print('no')
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
