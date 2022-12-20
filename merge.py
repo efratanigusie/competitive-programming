a733e5269b95c66d44603903b149e8dedec5bb5a
efratanigusie <119038187+efratanigusie@users.noreply.github.com>
2022-12-21 00:56:55 +0300

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort( key=lambda i:i[0])
+        output = [intervals[0]]
+        for start,end in intervals[1:]:
+            lastend = output[-1][1]
+            if start<=lastend:
+                output[-1][1] = max(lastend,end)
+            else:
+               output.append([start,end])
+        return output
