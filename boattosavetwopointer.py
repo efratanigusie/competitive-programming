440a3f5f6029e4b3984dd3d97dada135cc71d47d
efratanigusie <119038187+efratanigusie@users.noreply.github.com>
2023-02-12 14:21:05 +0300

    Create boattosavetwopointer.py

---
+++ boattosavetwopointer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        ans = 0
+        i = 0
+        j = len(people) - 1
+
+        people.sort()
+
+        while i <= j:
+            remain = limit - people[j]
+            j -= 1
+            if people[i] <= remain:
+                i += 1
+            ans += 1
+
+        return ans
